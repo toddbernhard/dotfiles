31e056c1ec2f78a9701ea329a28eabd4d48c6882
Todd Bernhard <toddb@minutekey.com>
2014-11-15 00:16:00 -0700

    mostly vim config

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,8 @@
 #!/usr/bin/env zsh
 
 alias la="ls -al"
+alias rn="ls"
 
-alias tma="tmux attach || tmux new-session -s first"
+alias svim="sudo vim"
+
+alias tma="tmux attach -d || tmux new-session -s first"
--- zsh/key_bindings.zsh
+++ zsh/key_bindings.zsh
@@ -1,39 +1,35 @@
 #!/usr/bin/env zsh
 
-# start typing + [Up-Arrow] - fuzzy find history forward
+
 if [[ "${terminfo[kcuu1]}" != "" ]]; then
-  bindkey "${terminfo[kcuu1]}" up-line-or-search 
+  bindkey "${terminfo[kcuu1]}" up-line-or-search          # start typing + [Up-Arrow] - fuzzy find history forward
 fi
 
-bindkey "^[[A" up-line-or-search
-
-# start typing + [Down-Arrow] - fuzzy find history backward
+if [[ "${terminfo[kcuu1]}" != "" ]]; then
+        bindkey "${terminfo[kcuu1]}" up-line-or-search    # start typing + [Up-Arrow] - fuzzy find history forward
+fi
 if [[ "${terminfo[kcud1]}" != "" ]]; then
-  bindkey "${terminfo[kcud1]}" down-line-or-search
+        bindkey "${terminfo[kcud1]}" down-line-or-search  # start typing + [Down-Arrow] - fuzzy find history backward
 fi
-
-bindkey "^[[B" down-line-or-search
-
-# [Home] - Go to beginning of line
 if [[ "${terminfo[khome]}" != "" ]]; then
-  bindkey "${terminfo[khome]}" beginning-of-line
+        bindkey "${terminfo[khome]}" beginning-of-line    # [Home] - Go to beginning of line
 fi
 if [[ "${terminfo[kend]}" != "" ]]; then
-  bindkey "${terminfo[kend]}"  end-of-line            # [End] - Go to end of line
+        bindkey "${terminfo[kend]}" end-of-line           # [End] - Go to end of line
 fi
 
-bindkey ' ' magic-space                               # [Space] - do history expansion
+bindkey ' ' magic-space                                   # [Space] - do history expansion
 
-bindkey '^[[1;5C' forward-word                        # [Ctrl-RightArrow] - move forward one word
-bindkey '^[[1;5D' backward-word                       # [Ctrl-LeftArrow] - move backward one word
+bindkey '^[[1;5C' forward-word                            # [Ctrl-RightArrow] - move forward one word
+bindkey '^[[1;5D' backward-word                           # [Ctrl-LeftArrow] - move backward one word
 
 if [[ "${terminfo[kcbt]}" != "" ]]; then
-  bindkey "${terminfo[kcbt]}" reverse-menu-complete   # [Shift-Tab] - move through the completion menu backwards
+  bindkey "${terminfo[kcbt]}" reverse-menu-complete       # [Shift-Tab] - move through the completion menu backwards
 fi
 
-bindkey '^?' backward-delete-char                     # [Backspace] - delete backward
+bindkey '^?' backward-delete-char                         # [Backspace] - delete backward
 if [[ "${terminfo[kdch1]}" != "" ]]; then
-  bindkey "${terminfo[kdch1]}" delete-char            # [Delete] - delete forward
+  bindkey "${terminfo[kdch1]}" delete-char                # [Delete] - delete forward
 else
   bindkey "^[[3~" delete-char
   bindkey "^[3;5~" delete-char
