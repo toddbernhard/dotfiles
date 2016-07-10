4bb7146d01e4415a2ba4468c4ee3239943d7d25c
Todd Bernhard <bernhard.todd@gmail.com>
2016-07-10 12:41:23 -0600

    git aliases; zsh tab completion

--- zsh/misc.zsh
+++ zsh/misc.zsh
@@ -8,13 +8,13 @@ setopt appendhistory  # append rather than replace
 setopt HIST_FIND_NO_DUPS  # show commands only once when searching
 
 
-setopt autocd  # allows you to skip the cd command
+# setopt autocd  # allows you to skip the cd command
 setopt extendedglob  # enables things like "cp ^*.(tar|bz2|gz)"
 setopt dotglob  # show hidden files in tab completion
 unsetopt beep  # no beep?
-unsetopt auto_cd
 
 [[ "$TERM" == "xterm" ]] && export TERM=xterm-256color
+
 export SHELL=/bin/zsh
 export EDITOR=/usr/bin/vim
 
@@ -24,4 +24,8 @@ export ACK_COLOR_MATCH="bold cyan"
 export ACK_COLOR_LINENO="yellow"
 export ACK_COLOR_FILENAME="blue"
 
+# Vim bindings
 bindkey -v
+
+# Autocompletion plugins
+autoload -Uz compinit && compinit
