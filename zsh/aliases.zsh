b3f5331dd1817228b57c7a4b9b2f17f673ef151f
Todd Bernhard <tbernhard@lifetimefitness.com>
2016-07-30 11:48:23 -0600

    docker aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,11 +1,16 @@
 #!/usr/bin/env zsh
 
+# LS
 alias la="ls -al"
+alias l1="ls -1"
 alias rn="ls" # typo
 alias /-="ls" # typo
 
-alias dorker="docker" # typo
-alias dokcer="docker" # typo
+# CD
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+
 
 alias dud="du -h -c -d "  # disk usage depth
 alias df="df -h"
@@ -19,6 +24,18 @@ alias mux="mux2.0"  # tmuxinator
 alias tree-src="tree -I \"node_modules|lib|target\""
 alias source-zsh="source ~/.zshrc"
 
-alias ..="cd .."
-alias ...="cd ../.."
-alias ....="cd ../../.."
+
+# DOCKER
+alias dorker='docker'
+alias dokcer='docker'
+alias dokcr='docker'
+alias dockr='docker'
+alias dockre='docker'
+
+alias dpsa='docker ps -a | grep -v alpine'
+alias dlf='docker logs -f'
+alias docker-rm-all='docker rm -f $(docker ps -aq); docker ps -a'
+alias docker-rm-all-images='docker-rm-all; docker rmi -f  $(docker images -q); docker images'
+function docker-exec {
+  docker exec -it $1 bash
+}
