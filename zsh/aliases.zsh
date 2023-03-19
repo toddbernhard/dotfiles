d4f45a9d4261cc4cd6b377af1c9929c8ea407148
Todd Bernhard <todd.bernhard@uplight.com>
2023-03-19 13:00:29 -0600

    Clean up vim and zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,8 +1,15 @@
 #!/usr/bin/env zsh
 
+# If not running interactively, do nothing
+if ! [[ -o login ]]; then
+    return
+fi
+
 # LS
-alias la="ls -al"
-alias l1="ls -1"
+alias lss='ls -CF'  # -C columns, -F add annotations like / or *
+alias ll='ls -AlFh' # -l list format, -h human sizes
+alias la="ls -A"    # -A show hidden but not . or ..
+alias l1="ls -1"    # -1 just the names
 alias rn="ls" # typo
 alias /-="ls" # typo
 
@@ -10,18 +17,34 @@ alias /-="ls" # typo
 alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
+alias cd..="cd .."
+alias cd...="cd ../.."
+alias cd-="cd -"
 
+alias mkdir='mkdir -p -v' # -p make intermediate directories, -v list each created director
 
 alias dud="du -h -c -d "  # disk usage depth
+alias du='du -h'
 alias df="df -h"
+
+alias less='less -Fi' # -F quit if one screen, -i ignore case during search
 
 alias svim="sudo vim"
 
 alias tma="tmux attach -d || tmux new-session"
 alias tmaa="tmux attach -d || tmux new-session -s "
 
+alias tree="tree -nF"
 alias tree-src="tree -I \"node_modules|lib|target\""
+
 alias source-zsh="source ~/.zshrc"
+
+# Safety features
+# Generally, check before overwriting
+alias cp='cp -i'
+alias mv='mv -i'
+alias rm='rm -I'  # check when deleting more than 3 or a recursive directory
+alias ln='ln -i'
 
 # APT
 alias apt-full-upgrade="sudo apt update && sudo apt upgrade && sudo apt dist-upgrade && sudo apt autoremove"
@@ -41,12 +64,10 @@ function docker-exec {
   docker exec -it $1 bash
 }
 
+# alias reboot='sudo systemctl reboot'
+# alias poweroff='sudo systemctl poweroff'
 
-function vertc-lines {
- awk -F\, '{for(i=1;i<=NF;i++) printf("%3d  %s\n",i,$i)}'
-}
-
-function vertc {
- awk -F\, '{for(i=1;i<=NF;i++) printf("%s\n",$i)}'
-}
+## Toggle Dvorak ##
+# alias asdf="setxkbmap dvorak"
+# alias aoeu="setxkbmap qwerty"
 
--- zsh/misc.zsh
+++ zsh/misc.zsh
@@ -1,5 +1,15 @@
 #!/usr/bin/env zsh
 
+export SHELL=/bin/zsh
+export EDITOR=/usr/bin/vim
+
+export PATH="$HOME"/bin:/sbin:/usr/sbin:"$PATH"
+
+# Login shells only past this point
+if ! [[ -o login ]]; then
+    return
+fi
+
 # history
 HISTFILE=~/.zhistory
 HISTSIZE=1000
@@ -7,22 +17,10 @@ SAVEHIST=1000
 setopt appendhistory  # append rather than replace
 setopt HIST_FIND_NO_DUPS  # show commands only once when searching
 
-
 # setopt autocd  # allows you to skip the cd command
 setopt extendedglob  # enables things like "cp ^*.(tar|bz2|gz)"
 setopt dotglob  # show hidden files in tab completion
 unsetopt beep  # no beep?
-
-[[ "$TERM" == "xterm" ]] && export TERM=xterm-256color
-
-export SHELL=/bin/zsh
-export EDITOR=/usr/bin/vim
-
-export PATH="$HOME"/bin:/sbin:/usr/sbin:"$PATH"
-
-export ACK_COLOR_MATCH="bold cyan"
-export ACK_COLOR_LINENO="yellow"
-export ACK_COLOR_FILENAME="blue"
 
 # Vim bindings
 bindkey -v
@@ -34,3 +32,15 @@ autoload -Uz compinit && compinit
 if type "pyenv" > /dev/null; then
   eval "$(pyenv init -)"
 fi
+
+# Colors
+[[ "$TERM" == "xterm" ]] && export TERM=xterm-256color
+
+export CLICOLOR="Yes"
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+
+export ACK_COLOR_MATCH="bold cyan"
+export ACK_COLOR_LINENO="yellow"
+export ACK_COLOR_FILENAME="blue"
